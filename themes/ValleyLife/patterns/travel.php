43f3b1b5b40d7f8a74c18c00cd251c2b0a12d0ba
Aranarora <aranarora007@gmail.com>
2024-02-21 19:46:21 -0800

    Make travel page with cover image, header & footer

---
+++ themes/ValleyLife/patterns/travel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: travel
+ * Slug: /travel
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header-copy"} /-->
+
+<!-- wp:group {"tagName":"main"} -->
+<main class="wp-block-group"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/devon-hawkins-5yQL5QYiP0g-unsplash-scaled.jpg","id":49,"dimRatio":40,"minHeight":344,"minHeightUnit":"px","align":"full","style":{"spacing":{"padding":{"top":"48px","right":"48px","bottom":"48px","left":"48px"},"margin":{"top":"0"}},"color":{"duotone":"unset"}}} -->
+<div class="wp-block-cover alignfull" style="margin-top:0;padding-top:48px;padding-right:48px;padding-bottom:48px;padding-left:48px;min-height:344px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-40 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-49" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/devon-hawkins-5yQL5QYiP0g-unsplash-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"48px"}}} -->
+<h2 class="wp-block-heading has-text-align-center" style="font-size:48px">Travel to the Valley</h2>
+<!-- /wp:heading --></div></div>
+<!-- /wp:cover --></main>
+<!-- /wp:group -->
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
