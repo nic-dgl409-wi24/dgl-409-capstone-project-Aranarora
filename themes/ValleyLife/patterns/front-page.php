418e1a492ff16764b0a08c79f5b19952aef20bab
Aranarora <aranarora007@gmail.com>
2024-02-21 18:00:32 -0800

    Add the cards in the front page relates to setteling

--- themes/ValleyLife/patterns/front-page.php
+++ themes/ValleyLife/patterns/front-page.php
@@ -8,12 +8,88 @@
 ?>
 <!-- wp:template-part {"slug":"header"} /-->
 
-<!-- wp:nextend/smartslider3 {"slider":"2"} -->
-<div class="wp-block-nextend-smartslider3">[smartslider3 slider="2"]</div>
-<!-- /wp:nextend/smartslider3 -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Settling</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"color":{"text":"#000000"},"elements":{"link":{"color":{"text":"#000000"}}},"spacing":{"padding":{"top":"2em","right":"2em","bottom":"2em","left":"2em"}},"border":{"width":"1px"}},"borderColor":"contrast","backgroundColor":"base"} -->
+<div class="wp-block-column has-border-color has-contrast-border-color has-base-background-color has-text-color has-background has-link-color" style="border-width:1px;color:#000000;padding-top:2em;padding-right:2em;padding-bottom:2em;padding-left:2em"><!-- wp:outermost/icon-block {"iconName":"wordpress-home","itemsJustification":"center","iconColor":"contrast","iconColorValue":"#1a6987","width":"100px"} -->
+<div class="wp-block-outermost-icon-block items-justified-center"><div class="icon-container has-icon-color has-contrast-color" style="color:#1a6987;width:100px"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 4L4 7.9V20h16V7.9L12 4zm6.5 14.5H14V13h-4v5.5H5.5V8.8L12 5.7l6.5 3.1v9.7z"></path></svg></div></div>
+<!-- /wp:outermost/icon-block -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontSize":"28px"}},"textColor":"contrast"} -->
+<h3 class="wp-block-heading has-text-align-center has-contrast-color has-text-color has-link-color" id="single" style="font-size:28px">Accomodation</h3>
+<!-- /wp:heading -->
+
+<!-- wp:separator {"opacity":"css","style":{"color":{"background":"#000000"}},"className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-css-opacity has-background is-style-wide" style="background-color:#000000;color:#000000"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Accommodation services are tailored to meet the diverse needs of international students and newcomers in the Comox Valley</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"align":"full","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons alignfull"><!-- wp:button {"textColor":"primary","width":100,"style":{"border":{"radius":"100px"}},"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link has-primary-color has-text-color wp-element-button" style="border-radius:100px">More Info</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"color":{"text":"#000000"},"elements":{"link":{"color":{"text":"#000000"}}},"spacing":{"padding":{"top":"2em","right":"2em","bottom":"2em","left":"2em"}},"border":{"width":"2px"}},"borderColor":"contrast","backgroundColor":"base"} -->
+<div class="wp-block-column has-border-color has-contrast-border-color has-base-background-color has-text-color has-background has-link-color" style="border-width:2px;color:#000000;padding-top:2em;padding-right:2em;padding-bottom:2em;padding-left:2em"><!-- wp:outermost/icon-block {"iconName":"wordpress-inbox","itemsJustification":"center","iconColor":"contrast","iconColorValue":"#1a6987","width":"100px"} -->
+<div class="wp-block-outermost-icon-block items-justified-center"><div class="icon-container has-icon-color has-contrast-color" style="color:#1a6987;width:100px"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M6 5.5h12a.5.5 0 01.5.5v7H14a2 2 0 11-4 0H5.5V6a.5.5 0 01.5-.5zm-.5 9V18a.5.5 0 00.5.5h12a.5.5 0 00.5-.5v-3.5h-3.337a3.5 3.5 0 01-6.326 0H5.5zM4 13V6a2 2 0 012-2h12a2 2 0 012 2v12a2 2 0 01-2 2H6a2 2 0 01-2-2v-5z" clip-rule="evenodd"></path></svg></div></div>
+<!-- /wp:outermost/icon-block -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontSize":"28px"}},"textColor":"contrast"} -->
+<h3 class="wp-block-heading has-text-align-center has-contrast-color has-text-color has-link-color" id="family" style="font-size:28px">Looking for Job</h3>
+<!-- /wp:heading -->
+
+<!-- wp:separator {"opacity":"css","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-css-opacity is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Job search experience for newcomers, offering access to local job listings, effective strategies, and networking opportunities</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"align":"full","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons alignfull"><!-- wp:button {"backgroundColor":"base","textColor":"primary","width":100,"style":{"border":{"radius":"100px"}},"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link has-primary-color has-base-background-color has-text-color has-background wp-element-button" style="border-radius:100px">More Info</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"color":{"text":"#000000"},"elements":{"link":{"color":{"text":"#000000"}}},"spacing":{"padding":{"top":"2em","right":"2em","bottom":"2em","left":"2em"}},"border":{"width":"2px"}},"borderColor":"contrast","backgroundColor":"base"} -->
+<div class="wp-block-column has-border-color has-contrast-border-color has-base-background-color has-text-color has-background has-link-color" style="border-width:2px;color:#000000;padding-top:2em;padding-right:2em;padding-bottom:2em;padding-left:2em"><!-- wp:outermost/icon-block {"iconName":"wordpress-cover","itemsJustification":"center","iconColor":"contrast","iconColorValue":"#1a6987","width":"100px"} -->
+<div class="wp-block-outermost-icon-block items-justified-center"><div class="icon-container has-icon-color has-contrast-color" style="color:#1a6987;width:100px"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M18.7 3H5.3C4 3 3 4 3 5.3v13.4C3 20 4 21 5.3 21h13.4c1.3 0 2.3-1 2.3-2.3V5.3C21 4 20 3 18.7 3zm.8 15.7c0 .4-.4.8-.8.8H5.3c-.4 0-.8-.4-.8-.8V5.3c0-.4.4-.8.8-.8h6.2v8.9l2.5-3.1 2.5 3.1V4.5h2.2c.4 0 .8.4.8.8v13.4z"></path></svg></div></div>
+<!-- /wp:outermost/icon-block -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontSize":"28px"}},"textColor":"contrast"} -->
+<h3 class="wp-block-heading has-text-align-center has-contrast-color has-text-color has-link-color" id="patron" style="font-size:28px"><strong>Academic Resources</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:separator {"opacity":"css","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-css-opacity is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Providing essential information and resources, including visa guidance and academic support for international students and newcomers.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"align":"full","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons alignfull"><!-- wp:button {"textColor":"primary","width":100,"style":{"border":{"radius":"100px","width":"2px"}},"borderColor":"primary","className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link has-primary-color has-text-color has-border-color has-primary-border-color wp-element-button" style="border-width:2px;border-radius:100px">More Info</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:media-text {"mediaId":46,"mediaLink":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/artboard-2-fotor-bg-remover-20240221133057","mediaType":"image"} -->
+<div class="wp-block-group"><!-- wp:media-text {"align":"","mediaId":46,"mediaLink":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/artboard-2-fotor-bg-remover-20240221133057","mediaType":"image"} -->
 <div class="wp-block-media-text is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Artboard-2-fotor-bg-remover-20240221133057.png" alt="" class="wp-image-46 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary","fontSize":"large"} -->
 <h2 class="wp-block-heading has-secondary-color has-text-color has-link-color has-large-font-size">About Us</h2>
 <!-- /wp:heading -->
@@ -27,8 +103,8 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"textColor":"contrast","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"className":"is-style-outline"} -->
-<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-contrast-color has-text-color has-link-color wp-element-button">Continue Reading</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"primary","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-primary-color has-text-color has-link-color wp-element-button">Continue Reading</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div></div>
 <!-- /wp:media-text --></div>
