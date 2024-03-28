11abe3dc4abb9929921351a948135001f7d8481b
Aranarora <aranarora007@gmail.com>
2024-03-28 00:00:42 -0700

    Update the plugins and check that all are compatible

--- uploads/pum/pum-site-scripts.js
+++ uploads/pum/pum-site-scripts.js
@@ -1,6 +1,6 @@
 /**
  * Do not touch this file! This file created by the Popup Maker plugin using PHP
- * Last modified time: Mar 03 2024, 07:52:03
+ * Last modified time: Mar 18 2024, 08:38:40
  */
 
 
